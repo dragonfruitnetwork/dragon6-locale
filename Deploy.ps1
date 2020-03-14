d551b433457cfbe3aaf982482588cbf1f06f262a
Albie <admin@dragonfruit.network>
2020-03-14 13:44:28 +0000

    fix deploy script service endpoint

--- Deploy.ps1
+++ Deploy.ps1
@@ -2,11 +2,9 @@ $VERSION = Get-Date -UFormat "%Y.%m.%d"
 $TRAVIS_NO = $env:TRAVIS_BUILD_NUMBER
 $NUGET = $env:NUGET_KEY
 
-nuget sources add -Name NuGet -Source "https://api.nuget.org/v3/index.json"
-
 dotnet restore ".\DragonFruit.Six.Locale.sln"
 dotnet pack ".\DragonFruit.Six.Locale\DragonFruit.Six.Locale.csproj" -o .\ -c Release -p:PackageVersion="$VERSION.$TRAVIS_NO"
 
 Get-ChildItem -Path .\ -Filter *.nupkg -Recurse -File -Name | ForEach-Object {
-    nuget push $_ -k $NUGET -s NuGet --skip-duplicate -n --force-english-output
+    nuget push $_ -k $NUGET -s "https://api.nuget.org/v3/index.json" --skip-duplicate -n --force-english-output --no-service-endpoint
 }
