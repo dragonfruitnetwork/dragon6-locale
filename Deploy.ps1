3c9d7621e8357269cda528f9afd370ff4d548923
Albie <admin@dragonfruit.network>
2020-03-14 12:34:29 +0000

    Update Deploy.ps1

--- Deploy.ps1
+++ Deploy.ps1
@@ -1,6 +1,7 @@
-﻿$VERSION = Get-Date -UFormat "%Y.%m.%d"
+$VERSION = Get-Date -UFormat "%Y.%m.%d"
 $TRAVIS_NO = $env:TRAVIS_BUILD_NUMBER
+$NUGET = $env:NUGET_KEY
 
 dotnet restore ".\DragonFruit.Six.Locale.sln"
 dotnet pack ".\DragonFruit.Six.Locale\DragonFruit.Six.Locale.csproj" -o "..\" -c Release -p:PackageVersion="$VERSION.$TRAVIS_NO"
-dotnet push *.nupkg -k %NUGET_KEY% -s "https://api.nuget.org/v3/index.json" --skip-duplicate -n
+dotnet push *.nupkg -k $NUGET -s "https://api.nuget.org/v3/index.json" --skip-duplicate -n
