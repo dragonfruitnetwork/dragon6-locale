b9e362f72e1d0cf59501c49b44370f62043996b2
Albie <admin@dragonfruit.network>
2020-03-14 14:37:11 +0000

    Update Deploy.ps1

--- Deploy.ps1
+++ Deploy.ps1
@@ -5,11 +5,6 @@ $WORKINGDIR = Get-Location
 dotnet restore ".\DragonFruit.Six.Locale.sln"
 dotnet pack ".\DragonFruit.Six.Locale\DragonFruit.Six.Locale.csproj" -o $WORKINGDIR -c Release -p:PackageVersion="$VERSION.$env:TRAVIS_BUILD_NUMBER"
 
-nuget setApiKey $env:NUGET_KEY -source "api.nuget.org"
-nuget setApiKey $env:NUGET_KEY -source "www.nuget.org"
-nuget setApiKey $env:NUGET_KEY -source "nuget.org"
-nuget setApiKey $env:NUGET_KEY #sourceless
-
 Get-ChildItem -Path $WORKINGDIR -Filter *.nupkg -Recurse -File -Name | ForEach-Object {
-    nuget push $_ -Source $NUGET_SOURCE -SkipDuplicate -NoSymbols
+    nuget push $_ -Verbosity detailed -ApiKey $env:NUGET_KEY -Source $NUGET_SOURCE
 }
