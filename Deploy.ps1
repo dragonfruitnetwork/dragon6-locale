b5ed082b3001988438b63dcc437a686967927509
Albie <admin@dragonfruit.network>
2020-03-14 13:26:49 +0000

    Update Deploy.ps1

--- Deploy.ps1
+++ Deploy.ps1
@@ -4,4 +4,7 @@ $NUGET = $env:NUGET_KEY
 
 dotnet restore ".\DragonFruit.Six.Locale.sln"
 dotnet pack ".\DragonFruit.Six.Locale\DragonFruit.Six.Locale.csproj" -o .\ -c Release -p:PackageVersion="$VERSION.$TRAVIS_NO"
-dotnet nuget push .\*.nupkg -k $NUGET -s "https://api.nuget.org/v3/index.json"
+
+Get-ChildItem -Path .\ -Filter *.nupkg -Recurse -File -Name | ForEach-Object {
+    dotnet nuget push $_ -k $NUGET -s https://api.nuget.org/v3/index.json --skip-duplicate -n --force-english-output
+}
