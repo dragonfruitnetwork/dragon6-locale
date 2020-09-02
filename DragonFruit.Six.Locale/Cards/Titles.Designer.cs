7e9b71caeffb5eaa0aa686a97710a107da0eda78
Albie <albion@dragonfruit.network>
2020-09-02 18:23:50 +0100

    add more terms and rank names

--- DragonFruit.Six.Locale/Cards/Titles.Designer.cs
+++ DragonFruit.Six.Locale/Cards/Titles.Designer.cs
@@ -19,7 +19,7 @@ namespace DragonFruit.Six.Locale.Cards {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "4.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     public class Titles {
@@ -84,6 +84,15 @@ namespace DragonFruit.Six.Locale.Cards {
         public static string DownsRevives {
             get {
                 return ResourceManager.GetString("DownsRevives", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Filter By.
+        /// </summary>
+        public static string FilterBy {
+            get {
+                return ResourceManager.GetString("FilterBy", resourceCulture);
             }
         }
         
@@ -219,6 +228,15 @@ namespace DragonFruit.Six.Locale.Cards {
         public static string SeasonWl {
             get {
                 return ResourceManager.GetString("SeasonWl", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Sort By.
+        /// </summary>
+        public static string SortBy {
+            get {
+                return ResourceManager.GetString("SortBy", resourceCulture);
             }
         }
         
