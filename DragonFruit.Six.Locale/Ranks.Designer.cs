3828e7a32053852f26fa4359374297f8dea9ed01
Albie <albion@dragonfruit.network>
2020-09-02 20:33:27 +0100

    make ranks access public

--- DragonFruit.Six.Locale/Ranks.Designer.cs
+++ DragonFruit.Six.Locale/Ranks.Designer.cs
@@ -19,10 +19,10 @@ namespace DragonFruit.Six.Locale {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "4.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    internal class Ranks {
+    public class Ranks {
         
         private static global::System.Resources.ResourceManager resourceMan;
         
@@ -36,7 +36,7 @@ namespace DragonFruit.Six.Locale {
         ///   Returns the cached ResourceManager instance used by this class.
         /// </summary>
         [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
-        internal static global::System.Resources.ResourceManager ResourceManager {
+        public static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
                     global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("DragonFruit.Six.Locale.Ranks", typeof(Ranks).Assembly);
@@ -51,7 +51,7 @@ namespace DragonFruit.Six.Locale {
         ///   resource lookups using this strongly typed resource class.
         /// </summary>
         [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
-        internal static global::System.Globalization.CultureInfo Culture {
+        public static global::System.Globalization.CultureInfo Culture {
             get {
                 return resourceCulture;
             }
@@ -63,7 +63,7 @@ namespace DragonFruit.Six.Locale {
         /// <summary>
         ///   Looks up a localized string similar to Bronze 1.
         /// </summary>
-        internal static string Bronze1 {
+        public static string Bronze1 {
             get {
                 return ResourceManager.GetString("Bronze1", resourceCulture);
             }
@@ -72,7 +72,7 @@ namespace DragonFruit.Six.Locale {
         /// <summary>
         ///   Looks up a localized string similar to Bronze 2.
         /// </summary>
-        internal static string Bronze2 {
+        public static string Bronze2 {
             get {
                 return ResourceManager.GetString("Bronze2", resourceCulture);
             }
@@ -81,7 +81,7 @@ namespace DragonFruit.Six.Locale {
         /// <summary>
         ///   Looks up a localized string similar to Bronze 3.
         /// </summary>
-        internal static string Bronze3 {
+        public static string Bronze3 {
             get {
                 return ResourceManager.GetString("Bronze3", resourceCulture);
             }
@@ -90,7 +90,7 @@ namespace DragonFruit.Six.Locale {
         /// <summary>
         ///   Looks up a localized string similar to Bronze 4.
         /// </summary>
-        internal static string Bronze4 {
+        public static string Bronze4 {
             get {
                 return ResourceManager.GetString("Bronze4", resourceCulture);
             }
@@ -99,7 +99,7 @@ namespace DragonFruit.Six.Locale {
         /// <summary>
         ///   Looks up a localized string similar to Bronze 5.
         /// </summary>
-        internal static string Bronze5 {
+        public static string Bronze5 {
             get {
                 return ResourceManager.GetString("Bronze5", resourceCulture);
             }
@@ -108,7 +108,7 @@ namespace DragonFruit.Six.Locale {
         /// <summary>
         ///   Looks up a localized string similar to Champion.
         /// </summary>
-        internal static string Champion {
+        public static string Champion {
             get {
                 return ResourceManager.GetString("Champion", resourceCulture);
             }
@@ -117,7 +117,7 @@ namespace DragonFruit.Six.Locale {
         /// <summary>
         ///   Looks up a localized string similar to Copper 1.
         /// </summary>
-        internal static string Copper1 {
+        public static string Copper1 {
             get {
                 return ResourceManager.GetString("Copper1", resourceCulture);
             }
@@ -126,7 +126,7 @@ namespace DragonFruit.Six.Locale {
         /// <summary>
         ///   Looks up a localized string similar to Copper 2.
         /// </summary>
-        internal static string Copper2 {
+        public static string Copper2 {
             get {
                 return ResourceManager.GetString("Copper2", resourceCulture);
             }
@@ -135,7 +135,7 @@ namespace DragonFruit.Six.Locale {
         /// <summary>
         ///   Looks up a localized string similar to Copper 3.
         /// </summary>
-        internal static string Copper3 {
+        public static string Copper3 {
             get {
                 return ResourceManager.GetString("Copper3", resourceCulture);
             }
@@ -144,7 +144,7 @@ namespace DragonFruit.Six.Locale {
         /// <summary>
         ///   Looks up a localized string similar to Copper 4.
         /// </summary>
-        internal static string Copper4 {
+        public static string Copper4 {
             get {
                 return ResourceManager.GetString("Copper4", resourceCulture);
             }
@@ -153,7 +153,7 @@ namespace DragonFruit.Six.Locale {
         /// <summary>
         ///   Looks up a localized string similar to Copper 5.
         /// </summary>
-        internal static string Copper5 {
+        public static string Copper5 {
             get {
                 return ResourceManager.GetString("Copper5", resourceCulture);
             }
@@ -162,7 +162,7 @@ namespace DragonFruit.Six.Locale {
         /// <summary>
         ///   Looks up a localized string similar to Diamond.
         /// </summary>
-        internal static string Diamond {
+        public static string Diamond {
             get {
                 return ResourceManager.GetString("Diamond", resourceCulture);
             }
@@ -171,7 +171,7 @@ namespace DragonFruit.Six.Locale {
         /// <summary>
         ///   Looks up a localized string similar to Gold 1.
         /// </summary>
-        internal static string Gold1 {
+        public static string Gold1 {
             get {
                 return ResourceManager.GetString("Gold1", resourceCulture);
             }
@@ -180,7 +180,7 @@ namespace DragonFruit.Six.Locale {
         /// <summary>
         ///   Looks up a localized string similar to Gold 2.
         /// </summary>
-        internal static string Gold2 {
+        public static string Gold2 {
             get {
                 return ResourceManager.GetString("Gold2", resourceCulture);
             }
@@ -189,7 +189,7 @@ namespace DragonFruit.Six.Locale {
         /// <summary>
         ///   Looks up a localized string similar to Gold 3.
         /// </summary>
-        internal static string Gold3 {
+        public static string Gold3 {
             get {
                 return ResourceManager.GetString("Gold3", resourceCulture);
             }
@@ -198,7 +198,7 @@ namespace DragonFruit.Six.Locale {
         /// <summary>
         ///   Looks up a localized string similar to Gold 4.
         /// </summary>
-        internal static string Gold4 {
+        public static string Gold4 {
             get {
                 return ResourceManager.GetString("Gold4", resourceCulture);
             }
@@ -207,7 +207,7 @@ namespace DragonFruit.Six.Locale {
         /// <summary>
         ///   Looks up a localized string similar to Platinum 1.
         /// </summary>
-        internal static string Plat1 {
+        public static string Plat1 {
             get {
                 return ResourceManager.GetString("Plat1", resourceCulture);
             }
@@ -216,7 +216,7 @@ namespace DragonFruit.Six.Locale {
         /// <summary>
         ///   Looks up a localized string similar to Platinum 2.
         /// </summary>
-        internal static string Plat2 {
+        public static string Plat2 {
             get {
                 return ResourceManager.GetString("Plat2", resourceCulture);
             }
@@ -225,7 +225,7 @@ namespace DragonFruit.Six.Locale {
         /// <summary>
         ///   Looks up a localized string similar to Platinum 3.
         /// </summary>
-        internal static string Plat3 {
+        public static string Plat3 {
             get {
                 return ResourceManager.GetString("Plat3", resourceCulture);
             }
@@ -234,7 +234,7 @@ namespace DragonFruit.Six.Locale {
         /// <summary>
         ///   Looks up a localized string similar to Silver 1.
         /// </summary>
-        internal static string Silver1 {
+        public static string Silver1 {
             get {
                 return ResourceManager.GetString("Silver1", resourceCulture);
             }
@@ -243,7 +243,7 @@ namespace DragonFruit.Six.Locale {
         /// <summary>
         ///   Looks up a localized string similar to Silver 2.
         /// </summary>
-        internal static string Silver2 {
+        public static string Silver2 {
             get {
                 return ResourceManager.GetString("Silver2", resourceCulture);
             }
@@ -252,7 +252,7 @@ namespace DragonFruit.Six.Locale {
         /// <summary>
         ///   Looks up a localized string similar to Silver 3.
         /// </summary>
-        internal static string Silver3 {
+        public static string Silver3 {
             get {
                 return ResourceManager.GetString("Silver3", resourceCulture);
             }
@@ -261,7 +261,7 @@ namespace DragonFruit.Six.Locale {
         /// <summary>
         ///   Looks up a localized string similar to Silver 4.
         /// </summary>
-        internal static string Silver4 {
+        public static string Silver4 {
             get {
                 return ResourceManager.GetString("Silver4", resourceCulture);
             }
@@ -270,7 +270,7 @@ namespace DragonFruit.Six.Locale {
         /// <summary>
         ///   Looks up a localized string similar to Silver 5.
         /// </summary>
-        internal static string Silver5 {
+        public static string Silver5 {
             get {
                 return ResourceManager.GetString("Silver5", resourceCulture);
             }
@@ -279,7 +279,7 @@ namespace DragonFruit.Six.Locale {
         /// <summary>
         ///   Looks up a localized string similar to Unranked.
         /// </summary>
-        internal static string Unranked {
+        public static string Unranked {
             get {
                 return ResourceManager.GetString("Unranked", resourceCulture);
             }
